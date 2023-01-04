82e305ee08d820a958aa6dd6ace95bbbae14440f
Oleksii_Pivniev <aapd8896@gmail.com>
2023-01-04 18:25:46 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,9 @@
 import throttle from 'lodash.throttle';
 
-// const formEl = document.querySelector('.feedback-form');
+const formEl = document.querySelector('.feedback-form');
 
-// const onContactFormItemInput = event => {
-//   const { target } = event;
+const onContactFormItemInput = event => {
+  const { target } = event;
 
-//   console.log(target);
-// }
+  console.log(target);
+}
